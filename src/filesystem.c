419f3ce9238ffdf7d7d969baee8dc6a7e259343f
na-razie-bez-nicku <87973435+na-razie-bez-nicku@users.noreply.github.com>
2026-02-16 15:39:55 +0100

    Update filesystem.c

--- src/filesystem.c
+++ src/filesystem.c
@@ -75,14 +75,29 @@ int fs_format(BlockDevice *dev)
     return 0;
 }
 
-int fs_verify(BlockDevice *dev)
+uint8_t *fs_verify(BlockDevice *dev)
 {
     uint8_t *header = (uint8_t *)malloc(512);
     block_read(dev, 0, header);
     header += 4; // Skip jump opcode
-    
+    if(memcmp(header, "SIMPLEFS", 8) != 0)
+    {
+        free(header - 4);
+        return NULL;
+    }
+    else
+    {
+        return header - 4;
+    }
 }
 
 int fs_mount(BlockDevice *dev, const char *uniqueName)
 {
+    uint8_t *headerBytes = fs_verify(dev);
+    if (headerBytes == NULL)
+        return -1;
+    fs_header header;
+    fs_header_from_bytes(&header, headerBytes);
+    free(headerBytes);
+    
 }
